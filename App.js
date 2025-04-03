b268742a82ae5abe4a0dea6a42abf90b4e1f1982
Rhogiel Andoy <theglitch0710@gmail.com>
2025-04-03 16:07:29 +0800

    useState Demonstation

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 
 export default function App() {
-  const [text, setText] = useState("Your input goes here"); // React Hooks [getter, setter]
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+
+  const [infoFirstName, setInfoFirstName] = useState("");
+  const [infoLastName, setInfoLastName] = useState("");
+
+  const saveInformation = () => {
+    setInfoFirstName(firstname);
+    setInfoLastName(lastname);
+  }
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.textinput}
-        placeholder="Enter your text here..."
-        onChangeText={(text) => setText(text)}
-      />
-      <Text style={styles.displayText}>You entered: {text}</Text>
+      <View style={{margin: 50}}>
+        <Text style={styles.displaySaved}>Firstname: {infoFirstName}</Text>
+        <Text style={styles.displaySaved}>Lastname:{infoLastName}</Text>
+      </View>
+      <Text>Firstname</Text>
+      <TextInput style={styles.textinput} placeholder="Enter firstname here..." onChangeText={(text) => setFirstname(text)} />
+      <Text>Lastname</Text>
+      <TextInput style={styles.textinput} placeholder="Enter lastname here..." onChangeText={(text) => setLastname(text)} />
+      <TouchableOpacity style={styles.button} onPress={() => saveInformation()}>
+        <Text style={styles.buttonText}>Submit Information</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -34,5 +48,19 @@ const styles = StyleSheet.create({
   },
   displayText: {
     fontSize: 20
+  },
+  button: {
+    backgroundColor: '#183B4E',
+    width: 300,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 15,
+  },
+  displaySaved: {
+    fontSize: 30,
   }
 });
