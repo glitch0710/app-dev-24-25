145b2922fcb92b92561acb771c56fcb2626fbfed
Rhogiel Andoy <theglitch0710@gmail.com>
2025-04-03 15:42:15 +0800

    Inputs

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [text, setText] = useState("Your input goes here"); // React Hooks [getter, setter]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.textinput}
+        placeholder="Enter your text here..."
+        onChangeText={(text) => setText(text)}
+      />
+      <Text style={styles.displayText}>You entered: {text}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F5EEDC",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  textinput: {
+    borderWidth: 1,
+    width: 300,
+    margin: 20,
+    color: "blue",
+    fontSize: 15,
+  },
+  displayText: {
+    fontSize: 20
+  }
 });
